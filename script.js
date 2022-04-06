cf81b0dfac58cd57bc8bc0310c364e7ad0b375c5
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-04-06 23:22:21 +0200

    🎨 Formatted code

--- script.js
+++ script.js
@@ -12,27 +12,27 @@ var generateButton = document.getElementById("generate");
 var selector = document.getElementById("selector");
 
 generateButton.onclick = () => {
-    if (selector.selectedIndex === 0) {
-        var url = "https://api.footapi.cf/clubs/random/ligue-1";
-    } else if (selector.selectedIndex === 1) {
-        var url = "https://api.footapi.cf/clubs/random/premier-league";
-    } else if (selector.selectedIndex === 2) {
-        var url = "https://api.footapi.cf/clubs/random/la-liga";
-    }
-    club1.style.display = "block";
-    club2.style.display = "block";
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            var data = data.random;
-            nameClub1.innerText = data.name;
-            imageClub1.src = data.image;
-        });
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            var data = data.random;
-            nameClub2.innerText = data.name;
-            imageClub2.src = data.image;
-        });
-}
+  if (selector.selectedIndex === 0) {
+    var url = "https://api.footapi.cf/clubs/random/ligue-1";
+  } else if (selector.selectedIndex === 1) {
+    var url = "https://api.footapi.cf/clubs/random/premier-league";
+  } else if (selector.selectedIndex === 2) {
+    var url = "https://api.footapi.cf/clubs/random/la-liga";
+  }
+  club1.style.display = "block";
+  club2.style.display = "block";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      var data = data.random;
+      nameClub1.innerText = data.name;
+      imageClub1.src = data.image;
+    });
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      var data = data.random;
+      nameClub2.innerText = data.name;
+      imageClub2.src = data.image;
+    });
+};
